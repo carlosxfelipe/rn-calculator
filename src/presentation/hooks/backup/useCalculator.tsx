975bb8bff8fa95e5c2142d3a26872e7304ba9e2d
Carlos Felipe Araujo <carlosxfelipe@gmail.com>
2024-08-04 20:18:44 -0300

    Backup old version

---
+++ src/presentation/hooks/backup/useCalculator.tsx
@@ -0,0 +1,157 @@
+// import {useRef, useState} from 'react';
+
+// enum Operator {
+//   add = '+',
+//   subtract = '-',
+//   multiply = 'x',
+//   divide = '÷',
+// }
+
+// export const useCalculator = () => {
+//   const [displayValue, setDisplayValue] = useState('0');
+//   const [previousValue, setPreviousValue] = useState('0');
+
+//   const lastOperation = useRef<Operator>();
+
+//   const buildNumber = (newValue: string) => {
+//     // Previne adição de um ponto decimal se já houver um no displayValue
+//     if (displayValue.includes('.') && newValue === '.') {
+//       return;
+//     }
+
+//     // Verifica se o displayValue começa com '0' ou '-0'
+//     if (displayValue.startsWith('0') || displayValue.startsWith('-0')) {
+//       // Permite adicionar um ponto decimal após '0' ou '-0'
+//       if (newValue === '.') {
+//         return setDisplayValue(displayValue + newValue);
+//       }
+
+//       // Permite adicionar '0' após um ponto decimal
+//       if (newValue === '0' && displayValue.includes('.')) {
+//         return setDisplayValue(displayValue + newValue);
+//       }
+
+//       // Substitui o '0' inicial por outro número, se não houver ponto decimal
+//       if (newValue !== '0' && !displayValue.includes('.')) {
+//         return setDisplayValue(newValue);
+//       }
+
+//       // Impede adicionar mais zeros à esquerda se ainda não houver ponto decimal
+//       if (newValue === '0' && !displayValue.includes('.')) {
+//         return;
+//       }
+
+//       // Adiciona o novo valor ao displayValue atual
+//       return setDisplayValue(displayValue + newValue);
+//     }
+
+//     // Caso padrão: adiciona o novo valor ao displayValue
+//     setDisplayValue(displayValue + newValue);
+//   };
+
+//   const toggleSign = () => {
+//     if (displayValue.includes('-')) {
+//       return setDisplayValue(displayValue.replace('-', ''));
+//     }
+
+//     setDisplayValue('-' + displayValue);
+//   };
+
+//   const clean = () => {
+//     setDisplayValue('0');
+//     setPreviousValue('0');
+//   };
+
+//   const deleteLastNumber = () => {
+//     if (
+//       displayValue.length === 1 ||
+//       (displayValue.length === 2 && displayValue.startsWith('-'))
+//     ) {
+//       return setDisplayValue('0');
+//     }
+
+//     // Remove o último caractere
+//     const newValue = displayValue.slice(0, -1);
+
+//     // Verifica se o novo valor termina com ponto decimal
+//     // e, em caso positivo, remove o ponto decimal também
+//     if (newValue.endsWith('.')) {
+//       return setDisplayValue(newValue.slice(0, -1));
+//     }
+
+//     setDisplayValue(newValue);
+//   };
+
+//   const setLastValue = () => {
+//     if (displayValue.endsWith('.')) {
+//       setPreviousValue(displayValue.slice(0, -1));
+//     } else {
+//       setPreviousValue(displayValue);
+//     }
+
+//     setDisplayValue('0');
+//   };
+
+//   const divisionOperation = () => {
+//     setLastValue();
+//     lastOperation.current = Operator.divide;
+//   };
+
+//   const multiplicationOperation = () => {
+//     setLastValue();
+//     lastOperation.current = Operator.multiply;
+//   };
+
+//   const subtractionOperation = () => {
+//     setLastValue();
+//     lastOperation.current = Operator.subtract;
+//   };
+
+//   const additionOperation = () => {
+//     setLastValue();
+//     lastOperation.current = Operator.add;
+//   };
+
+//   const calculateResult = () => {
+//     const num1 = Number(displayValue);
+
+//     const num2 = Number(previousValue);
+
+//     switch (lastOperation.current) {
+//       case Operator.add:
+//         setDisplayValue(`${num1 + num2}`);
+//         break;
+
+//       case Operator.subtract:
+//         setDisplayValue(`${num2 - num1}`);
+//         break;
+
+//       case Operator.multiply:
+//         setDisplayValue(`${num1 * num2}`);
+//         break;
+
+//       case Operator.divide:
+//         setDisplayValue(`${num2 / num1}`);
+//         break;
+//     }
+
+//     setPreviousValue('0');
+//   };
+
+//   return {
+//     // Properties
+//     displayValue,
+//     previousValue,
+
+//     // Methods
+//     buildNumber,
+//     toggleSign,
+//     clean,
+//     deleteLastNumber,
+//     divisionOperation,
+//     multiplicationOperation,
+//     subtractionOperation,
+//     additionOperation,
+//     calculateResult,
+//   };
+// };
---
+++ src/presentation/screens/backup/CalculatorScreen.tsx
@@ -0,0 +1,103 @@
+// /* eslint-disable react-native/no-inline-styles */
+// import React from 'react';
+// import {View, Text} from 'react-native';
+// import {colors, styles} from '../../config/theme/app-theme';
+// import {CalculatorButton} from '../components/CalculatorButton';
+// import {useCalculator} from '../hooks/useCalculator';
+
+// export const CalculatorScreen = (): JSX.Element => {
+//   const {
+//     displayValue,
+//     previousValue,
+//     buildNumber,
+//     toggleSign,
+//     clean,
+//     deleteLastNumber,
+//     divisionOperation,
+//     multiplicationOperation,
+//     subtractionOperation,
+//     additionOperation,
+//     calculateResult,
+//   } = useCalculator();
+
+//   return (
+//     <View style={styles.calculatorContainer}>
+//       <View style={{paddingHorizontal: 30, paddingBottom: 20}}>
+//         <Text adjustsFontSizeToFit numberOfLines={1} style={styles.mainResult}>
+//           {displayValue}
+//         </Text>
+//         <Text adjustsFontSizeToFit numberOfLines={1} style={styles.subResult}>
+//           {previousValue === '0' ? ' ' : previousValue}
+//         </Text>
+//       </View>
+//       <View style={styles.row}>
+//         <CalculatorButton
+//           onPress={clean}
+//           label="C"
+//           color={colors.lightGray}
+//           blackText
+//         />
+//         <CalculatorButton
+//           onPress={toggleSign}
+//           label="+/-"
+//           color={colors.lightGray}
+//           blackText
+//         />
+//         <CalculatorButton
+//           onPress={deleteLastNumber}
+//           label="del"
+//           color={colors.lightGray}
+//           blackText
+//         />
+//         <CalculatorButton
+//           onPress={divisionOperation}
+//           label="÷"
+//           color={colors.orange}
+//         />
+//       </View>
+//       <View style={styles.row}>
+//         <CalculatorButton onPress={() => buildNumber('7')} label="7" />
+//         <CalculatorButton onPress={() => buildNumber('8')} label="8" />
+//         <CalculatorButton onPress={() => buildNumber('9')} label="9" />
+//         <CalculatorButton
+//           onPress={multiplicationOperation}
+//           label="x"
+//           color={colors.orange}
+//         />
+//       </View>
+//       <View style={styles.row}>
+//         <CalculatorButton onPress={() => buildNumber('4')} label="4" />
+//         <CalculatorButton onPress={() => buildNumber('5')} label="5" />
+//         <CalculatorButton onPress={() => buildNumber('6')} label="6" />
+//         <CalculatorButton
+//           onPress={subtractionOperation}
+//           label="-"
+//           color={colors.orange}
+//         />
+//       </View>
+//       <View style={styles.row}>
+//         <CalculatorButton onPress={() => buildNumber('1')} label="1" />
+//         <CalculatorButton onPress={() => buildNumber('2')} label="2" />
+//         <CalculatorButton onPress={() => buildNumber('3')} label="3" />
+//         <CalculatorButton
+//           onPress={additionOperation}
+//           label="+"
+//           color={colors.orange}
+//         />
+//       </View>
+//       <View style={styles.row}>
+//         <CalculatorButton
+//           onPress={() => buildNumber('0')}
+//           label="0"
+//           doubleSize
+//         />
+//         <CalculatorButton onPress={() => buildNumber('.')} label="." />
+//         <CalculatorButton
+//           onPress={calculateResult}
+//           label="="
+//           color={colors.orange}
+//         />
+//       </View>
+//     </View>
+//   );
+// };
